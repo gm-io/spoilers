77ba15ab787cd1fde4f608ace1b73ddbfd4aaae1
Shomegie <shomegie@gmail.com>
2023-07-31 01:11:31 -0230

    add skeleton ui to project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,11 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+    // Your selected Skeleton theme:
+    import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+    // This contains the bulk of Skeletons required styles:
+    import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+    // Application's global stylesheet (sometimes labeled 'app.css')
+    import "../app.postcss";
+</script>
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+<div class="inset-0 absolute">
+    <div class="flex space-x-2 justify-end p-8">
+        <div class="">Willkommen Bei Spoilers</div>
+        <LightSwitch />
+    </div>
+
+</div>
