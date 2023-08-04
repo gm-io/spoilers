758a4304a3b8971bca7625611b1ad588d99fcfff
Shomegie <shomegie@gmail.com>
2023-08-03 23:53:21 -0230

    write ratings logic without image assets

---
+++ src/routes/+page.js
@@ -0,0 +1,11 @@
+// import { error } from '@sveltejs/kit';
+
+export function load() {
+    fetch
+
+    return {
+        title: 'Hello world!',
+        content: 'Welcome to our blog. Lorem ipsum dolor sit amet...'
+    };
+    // throw error(404, 'Not found');
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,32 @@
 <script>
     import { LightSwitch } from '@skeletonlabs/skeleton';
+    import { Ratings } from '@skeletonlabs/skeleton';
+
+    let value = { current: 4.5, max: 7 };
+
+
+    export let data;
+
+
+
+    function iconClick(event) {
+	    value.current = event.detail.index;
+    }
+						
 </script>
 <div class="inset-0 absolute">
-    <div class="flex space-x-2 justify-end p-8">
+    <div class="flex space-x-2 justify-end p-8 pb-0">
         <div class="">Willkommen Bei Spoilers</div>
         <LightSwitch />
+    </div>
+    <div class="">
+        {data.title}
+        <br>
+        <Ratings bind:value={value.current} max={value.max} interactive on:icon={iconClick}>
+            <svelte:fragment slot="empty">[ ]</svelte:fragment>
+            <svelte:fragment slot="half">[.5]</svelte:fragment>
+            <svelte:fragment slot="full">(X)</svelte:fragment>
+        </Ratings>
     </div>
 
 </div>
