80261e4a1c56000e1a1c6677d1fa16fdad0b1c08
Shomegie <shomegie@gmail.com>
2023-08-09 22:00:14 -0230

    change landing page carousel format

--- src/routes/design/+page.svelte
+++ src/routes/design/+page.svelte
@@ -10,8 +10,7 @@
     let trending_category_series = "weekly"
 
     export let data;
-    console.log(data.day_trends)
-
+    
     function iconClick(event) {
 	    value.current = event.detail.index;
     }				
@@ -24,7 +23,7 @@
                 <div class="">Willkommen Bei Spoilers</div>
                 <LightSwitch />
             </div>
-            <div class="movie-carousel">
+            <!-- <div class="movie-carousel">
                 <div class="flex w-11/12 mx-auto">
                     <div class="flex items-center flex-col mb-4">
                         <div class="font-bold h4">Trending/Movies</div>
@@ -48,14 +47,14 @@
                         </div>
                     {/each}
                 </div>
-            </div>
+            </div> -->
             <div class="tv-carousel mt-8">
                 <div class="flex w-11/12 mx-auto">
                     <div class="flex items-center flex-col mb-4">
-                        <div class="font-bold h4">Trending/<span class="h4">TV Series</span></div>
+                        <div class="font-bold h4 mb-2">Trending August 2023<span class="h4"></span></div>
                         <RadioGroup class="-ml-4"  padding="px-2 py-0" active="variant-filled-primary" hover="hover:variant-soft-primary">                    
-                            <RadioItem bind:group={trending_category_series} name="justify" value={"daily"}>Today</RadioItem>
-                            <RadioItem bind:group={trending_category_series} name="justify" value={"weekly"}>7 Days</RadioItem>
+                            <RadioItem bind:group={trending_category_series} name="justify" value={"daily"}>Movies</RadioItem>
+                            <RadioItem bind:group={trending_category_series} name="justify" value={"weekly"}>Tv Series</RadioItem>
                         </RadioGroup>
                     </div>
                 </div>
@@ -76,27 +75,6 @@
             </div>
         </div>
     </div>
-    <!-- <div class="flex space-x-2 justify-end p-8 pb-0">
-        <div class="">Willkommen Bei Spoilers</div>
-        <LightSwitch />
-    </div>
-    <div class="">
-        <div class="snap-x space-x-3 scroll-px-4 snap-mandatory scroll-smooth flex overflow-x-auto pb-2 gap-7 w-5/6 mx-auto">
-            {#each Array.from({ length: 8 }) as _ , i}
-                <div class="snap-center shrink-0 w-fit text-center" >
-                    <img class="h-72 w-48 rounded-xl" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${data.movies[i].poster_path}`} />
-                    <div class="h-12 w-48 mt-4 mb-1  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
-                        {#if data.movies[i].media_type == "tv"}
-                            <div class="p-0 m-0 text-xs">{data.movies[i].name}</div>
-                        {:else}
-                            <div class="p-0 m-0 text-xs">{data.movies[i].title}</div>
-                        {/if}
-
-                    </div>
-                </div>
-            {/each}
-        </div>
-    </div> -->
 </div>
 
 
