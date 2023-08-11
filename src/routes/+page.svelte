a6f7f46f5fd6a3e02d72fae0fc88c0ea5b263b8b
Shomegie <shomegie@gmail.com>
2023-08-11 19:00:50 -0230

    update index page from design route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,69 +1,60 @@
 <script>
     import { LightSwitch } from '@skeletonlabs/skeleton';
     import { Ratings } from '@skeletonlabs/skeleton';
+    import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+
+
 
     let value = { current: 4.5, max: 7 };
-
+    let tv_or_movie = "movie"
 
     export let data;
-    console.log(data.movies)
-
-
+    
     function iconClick(event) {
 	    value.current = event.detail.index;
-    }
-						
+    }				
 </script>
 <div class="inset-0 absolute">
-    <div class="bg-red-500"></div>
-    <div class="flex space-x-2 justify-end p-8 pb-0">
-        <div class="">Willkommen Bei Spoilers</div>
-        <LightSwitch />
-    </div>
-
-
-    <!-- <div class="">
-        <br>
-        <Ratings bind:value={value.current} max={value.max} interactive on:icon={iconClick}>
-            <svelte:fragment slot="empty">[ ]</svelte:fragment>
-            <svelte:fragment slot="half">[x]</svelte:fragment>
-            <svelte:fragment slot="full">(X)</svelte:fragment>
-        </Ratings>
-    </div> -->
-    <div class="">
-        <div class="snap-x space-x-3 scroll-px-4 snap-mandatory scroll-smooth flex overflow-x-auto pb-2 gap-7 w-5/6 mx-auto">
-            {#each Array.from({ length: 8 }) as _ , i}
-                <div class="snap-center shrink-0 w-fit text-center" >
-                    <img class="h-72 w-48 rounded-xl" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${data.movies[i].poster_path}`} />
-                    <div class="h-12 w-48 mt-4 mb-1  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
-                        {#if data.movies[i].media_type == "tv"}
-                        <div class="p-0 m-0 text-xs">{data.movies[i].name}</div>
-                        <!-- <div class="">{data.movies[i].poster_path}</div> -->
-                        {:else}
-                        <div class="p-0 m-0 text-xs">{data.movies[i].title}</div>
-                        <!-- <div class="">{data.movies[i].poster_path}</div> -->
-                        {/if}
-
+    <div class=" h-full flex">
+        <div class="aside bg-green-300 h-full w-0/12 md:w-4/12 lg:w-3/12"></div>
+        <div class="main h-full w-full md:w-8/12 lg:w-9/12">
+            <div class="flex space-x-2 justify-end p-8 pb-0">
+                <div class="">Willkommen Bei Spoilers</div>
+                <LightSwitch />
+            </div>
+            <div class="tv-carousel mt-8">
+                <div class="flex w-11/12 mx-auto">
+                    <div class="flex items-center flex-col mb-4">
+                        <div class="font-bold h4 mb-2">Trending August 2023<span class="h4"></span></div>
+                        <RadioGroup class="-ml-4"  padding="px-2 py-0" active="variant-filled-primary" hover="hover:variant-soft-primary">                    
+                            <RadioItem bind:group={tv_or_movie} name="justify" value={"movie"}>Movies</RadioItem>
+                            <RadioItem bind:group={tv_or_movie} name="justify" value={"tv"}>Tv Series</RadioItem>
+                        </RadioGroup>
                     </div>
-                    {#if data.movies[i].media_type == "tv"}
-                        <!-- <div class="">{data.movies[i].name}</div> -->
-                        <!-- <div class="">{data.movies[i].poster_path}</div> -->
-                    {:else}
-                        <!-- <div class="p-0 m-0 w-fit bg-red-600">{data.movies[i].title}</div> -->
-                        <!-- <div class="">{data.movies[i].poster_path}</div> -->
-                    {/if}
                 </div>
-            {/each}
-    <!-- 
-            {#each }
-            {/each} -->
-
+                <div class="snap-x space-x-1 scroll-px-4 snap-mandatory hide-scrollbar scroll-smooth flex overflow-x-auto gap-4 w-11/12 mx-auto">
+                    {#each data.week_trends as content}
+                        {#if content.media_type == "tv" && tv_or_movie == "tv"}
+                            <div class="snap-center shrink-0 w-fit text-center" >
+                                <img class="h-44 w-32 rounded-xl" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} />
+                                <div class="h-12 w-32 mt-4 mb-1  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
+                                        <div class="p-0 m-0 text-xs">{content.name}</div>
+                                </div>
+                            </div>  
+                        {/if}
+                        {#if content.media_type == "movie" && tv_or_movie == "movie"}
+                            <div class="snap-center shrink-0 w-fit text-center" >
+                                <img class="h-44 w-32 rounded-xl" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} />
+                                <div class="h-12 w-32 mt-4 mb-1  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
+                                        <div class="p-0 m-0 text-xs">{content.title}</div>
+                                </div>
+                            </div>  
+                        {/if}
+                    {/each}
+                </div>
+            </div>
         </div>
     </div>
-
-
-
-
 </div>
 
 
