654940efdcca70084d5747871477bcd240b03071
Shomegie <shomegie@gmail.com>
2023-08-14 00:51:20 -0230

    change dimensions of trending

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,17 +24,17 @@
                 <div class="">Willkommen Bei Spoilers</div>
                 <LightSwitch />
             </div>
-            <div class="searchbar bg-yellow-600 h-fit w-full">
+            <div class="searchbar h-fit w-full">
                 <SearchBar image_url={data.week_trends[6]} />
             </div>
 
-            <div class="tv-carousel mt-2 ">
+            <div class="tv-carousel mt-1 ">
                 <div class="flex w-11/12 mx-auto">
-                    <div class="flex items-center flex-col mb-4">
-                        <div class="font-bold h4 mb-2">Trending August 2023<span class="h4"></span></div>
+                    <div class="flex pl-3 flex-col mb-4 cursor-pointer">
+                        <!-- <div class="font-bold h mb-0">Trending August 2023<span class="h4"></span></div> -->
                         <RadioGroup class="-ml-4"  padding="px-2 py-0" active="variant-filled-primary" hover="hover:variant-soft-primary">  
-                            <RadioItem bind:group={tv_or_movie} name="justify" value={"movie"}>Movies</RadioItem>
-                            <RadioItem bind:group={tv_or_movie} name="justify" value={"tv"}>Tv Series</RadioItem>
+                            <RadioItem bind:group={tv_or_movie} name="justify" value={"movie"}>Trending Movies</RadioItem>
+                            <RadioItem bind:group={tv_or_movie} name="justify" value={"tv"}>Trending Series</RadioItem>
                         </RadioGroup>
                     </div>
                 </div>
@@ -42,16 +42,16 @@
                     {#each data.week_trends as content}
                         {#if content.media_type == "tv" && tv_or_movie == "tv"}
                             <div class="snap-center shrink-0 w-fit text-center" >
-                                <img class="h-44 w-32 rounded-xl" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} />
-                                <div class="h-12 w-32 mt-4 mb-1  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
+                                <img class="h-44 w-32 lg:h-64 lg:w-44 rounded-xl" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} />
+                                <div class="h-12 w-32 mt-4 mb-1 lg:w-44  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
                                         <div class="p-0 m-0 text-xs">{content.name}</div>
                                 </div>
                             </div>  
                         {/if}
                         {#if content.media_type == "movie" && tv_or_movie == "movie"}
                             <div class="snap-center shrink-0 w-fit text-center" >
-                                <img class="h-44 w-32 lg:h-72 lg:w-48 rounded-xl" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} />
-                                <div class="h-12 w-32 lg:w-48 mt-4 mb-1  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
+                                <img class="h-44 w-32 lg:h-64 lg:w-44 rounded-xl" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} />
+                                <div class="h-12 w-32 lg:w-44 mt-4 mb-1  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
                                         <div class="p-0 m-0 text-xs">{content.title}</div>
                                 </div>
                             </div>  
