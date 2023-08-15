1b538411617b522433fa61a0a5b8bf70d4e54c53
Shomegie <shomegie@gmail.com>
2023-08-15 05:18:30 -0230

    create landing backdrop component

---
+++ src/components/LandingBackdrop.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let image_url;
+</script>
+
+<div class="mb-4 relative">
+    <div class="absolute inset-0 flex">
+        <div class="w-10/12 text h-fit my-auto px-8 py-4 text-white bg-black bg-opacity-0">
+            <div class="flex flex-col space-y-4 p-2 bg-opacity-30">
+                <p class="h1 font-bold">{image_url.title}</p>
+                <p class=" text-xs w-full lg:w-2/3">{image_url.overview}</p>
+            </div>
+        </div>
+    </div>
+    <div class="h-96 bg-blac pl-6">
+        <img class="object-cover h-full w-full" alt="The project logo" src={`https://image.tmdb.org/t/p/w1280/i2GVEvltEu3BXn5crBSxgKuTaca.jpg`} />
+    </div>
+</div>
+<!-- <hr class="h-px ml-6 mt-3 bg-gray-200 border-0 dark:bg-gray-700"> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import { LightSwitch } from '@skeletonlabs/skeleton';
 	import Trending from '../components/Trending.svelte';
+	import LandingBackdrop from '../components/LandingBackdrop.svelte';
 
     export let data;
 
@@ -8,6 +9,9 @@
 
 <div class="">
     <LightSwitch />
+
+    <LandingBackdrop image_url={data.week_trends[7]} />
+
     <Trending trending_data={data} />
 
 </div>
