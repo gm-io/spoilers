61b68878e220b1603928f0e927834f061c66a73b
Shomegie <shomegie@gmail.com>
2023-08-15 05:07:25 -0230

    refactor trending carousel into its own component

---
+++ src/components/Trending.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let trending_data;
+    import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+
+    let tv_or_movie = "movie"
+
+</script>
+
+<div class="tv-carousel mt-1 ">
+    <div class="flex w-11/12 mx-auto">
+        <div class="flex pl-3 flex-col mb-4 cursor-pointer">
+            <!-- <div class="font-bold h mb-0">Trending August 2023<span class="h4"></span></div> -->
+            <RadioGroup class="-ml-4"  padding="px-2 py-0" active="variant-filled-primary" hover="hover:variant-soft-primary">  
+                <RadioItem bind:group={tv_or_movie} name="justify" value={"movie"}>Trending Movies</RadioItem>
+                <RadioItem bind:group={tv_or_movie} name="justify" value={"tv"}>Trending Series</RadioItem>
+            </RadioGroup>
+        </div>
+    </div>
+    <div class="snap-x space-x-1 scroll-px-4 snap-mandatory hide-scrollbar scroll-smooth flex overflow-x-auto gap-4 w-11/12 mx-auto">
+        {#each trending_data.week_trends as content}
+            {#if content.media_type == "tv" && tv_or_movie == "tv"}
+                <div class="snap-center shrink-0 w-fit text-center" >
+                    <img class="h-44 w-32 lg:h-64 lg:w-44 rounded-xl" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} />
+                    <div class="h-12 w-32 mt-4 mb-1 lg:w-44  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
+                            <div class="p-0 m-0 text-xs">{content.name}</div>
+                    </div>
+                </div>  
+            {/if}
+            {#if content.media_type == "movie" && tv_or_movie == "movie"}
+                <div class="snap-center shrink-0 w-fit text-center" >
+                    <img class="h-44 w-32 lg:h-64 lg:w-44 rounded-xl" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} />
+                    <div class="h-12 w-32 lg:w-44 mt-4 mb-1  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
+                            <div class="p-0 m-0 text-xs">{content.title}</div>
+                    </div>
+                </div>  
+            {/if}
+        {/each}
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-landing page
+<script>
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+	import Trending from '../components/Trending.svelte';
+
+    export let data;
+
+</script>
+
+<div class="">
+    <LightSwitch />
+    <Trending trending_data={data} />
+
+</div>
