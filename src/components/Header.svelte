c6fec582f51907ec3bddabf0db382909d8781a82
Shomegie <shomegie@gmail.com>
2023-08-16 04:51:46 -0230

    alternate layout proposal

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,1 +1,9 @@
-i am a header
+<script>
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+<div class="border-b w-full border-gray-200 border-opacity-10">
+    <div class="w-10/12 mx-auto p-3 flex justify-end relative">
+        <div class="absolute left-0 pl-3">Spoilers.IO</div>
+        <LightSwitch />
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script>
-    import { LightSwitch } from '@skeletonlabs/skeleton';
 	import Trending from '../components/Trending.svelte';
 	import LandingBackdrop from '../components/LandingBackdrop.svelte';
     import SearchBar from '../components/SearchBar.svelte';
     import Lhs from '../components/LHS.svelte';
 	import Header from '../components/Header.svelte';
-    Header
     export let data;
-
 </script>
 <!-- <div class="w-full lg:w-9/12 mx-auto flex flex-col">
     <LightSwitch />
@@ -17,15 +14,14 @@
         <Trending trending_data={data} />
     </div>
 </div> -->
-<Header />
 
+    <Header />
 <div class="w-10/12 mx-auto h-[100vh] flex">
     <div class="w-1/5 border-r border-black dark:border-gray-200 border-opacity-10 dark:border-opacity-10">
         <Lhs />
 
     </div>
     <div class="px-2 w-4/5">
-        <LightSwitch />
         <SearchBar />
         <LandingBackdrop image_url={data.week_trends[7]} />
         <div class="w-11/12 lg:w-full mx-auto ">
