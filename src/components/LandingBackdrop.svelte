b7d679e523bdaf757d7c83fe44173b2cb07995f1
Shomegie <shomegie@gmail.com>
2023-08-20 00:11:42 -0230

    complete animation of landingbackdrop component

--- src/components/LandingBackdrop.svelte
+++ src/components/LandingBackdrop.svelte
@@ -25,19 +25,18 @@
     console.log("backdrop data:",backdrop_data)
 
     let movie_day_trends = backdrop_data.day_trends.filter(x =>(x.media_type == "movie"))
-    movie_day_trends = movie_day_trends.slice(0,3)
                         
 </script>
 <!-- <progress class="rounded-none" value={$timer/original}></progress> -->
 
 {#key current}
-<div in:fade class="relative w-full">
-    <div class="mb-4 relative">
-        <div class="absolute right-0 z-50 w-fit flex items-center px-8 h-full">
-            <img class="h-80 w-64 rounde opacity-80 card drop-shadow-lg" src={`https://www.themoviedb.org/t/p/original${movie_day_trends[current].poster_path}`}  alt="">
+<div in:fade class="relative w-full px-2 bg-black mb-4 bg-opacity-10 rounded-lg">
+    <div class=" relative">
+        <div class="absolute right-0 z-50 w-fit flex items-center px-8 pr-14 h-full">
+            <img class="shadow-lg h-80 w-60 rounde opacity-80 card drop-shadow-lg" src={`https://www.themoviedb.org/t/p/original${movie_day_trends[current].poster_path}`}  alt="">
         </div>
         <div class="absolute inset-0 flex items-end">
-            <div class="w-[100%] text h-[100%] px-8 py-4 text-white bg-black bg-opacity-30 flex items-center">
+            <div class="w-[100%] text h-[100%] px-8 py-4 text-white bg-black bg-opacity-30 rounded-lg flex items-center">
                 <div class="flex flex-col space-y-4 p-2 h-fit">
                     <p class="h1 font-bold">{movie_day_trends[current].title}</p>
                     <p class=" text-xs w-full lg:w-2/3">{movie_day_trends[current].overview}</p>
@@ -52,7 +51,7 @@
             </div>
         </div>
         <div class="h-96 bg-blac">
-            <img class="object-cover h-full w-full rounde" alt="The project logo" src={`https://image.tmdb.org/t/p/original${movie_day_trends[current].backdrop_path}`} />
+            <img class=" rounded-lg object-cover h-full w-full rounde" alt="The project logo" src={`https://image.tmdb.org/t/p/original${movie_day_trends[current].backdrop_path}`} />
         </div>
     </div>
 </div>
