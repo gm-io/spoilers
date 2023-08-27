0012324d3afe816765124b477662ed0f9cb0ca54
Shomegie <shomegie@gmail.com>
2023-08-27 21:01:25 -0230

    add theme 3 and theme 4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
     // using custom theme; loaded here ->
-    import '../theme2.postcss';
+    import '../theme4.postcss';
 
     // This contains the bulk of Skeletons required styles:
     import '@skeletonlabs/skeleton/styles/skeleton.css';
