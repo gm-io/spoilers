192a85e28415fa2b7e6758b190fa82599eaf4fd4
Shomegie <shomegie@gmail.com>
2023-08-29 19:28:16 -0230

    fix tabbing and enter keyup functions of trending slider

--- src/components/Trending.svelte
+++ src/components/Trending.svelte
@@ -8,6 +8,10 @@
     let goto_media_page = (id_number,media_type) =>{
         goto(`/${media_type}/${id_number}`)
     }
+    let goto_media_page_keyHandler = (id_number,media_type,e) =>{
+        if (e.key == 'Enter' )
+            goto(`/${media_type}/${id_number}`)
+    }
 
 </script>
 
@@ -26,7 +30,7 @@
     <div class="snap-x space-x-2 scroll-px-4 snap-mandatory hide-scrollbar scroll-smooth flex overflow-x-auto gap-4 w-full mx-auto">
         {#each trending_data.week_trends as content}
             {#if content.media_type == "movie" && tv_or_movie == "movie"}
-                <div aria-pressed="false" role="button" tabindex="0" on:click={()=>{goto_media_page(content.id,'movie')}} on:keyup|preventDefault={()=>{goto_media_page(content.id,'movie')}} class="snap-center shrink-0 w-fit text-center cursor-pointer" >
+                <div aria-pressed="false" role="button" tabindex="0" on:click={()=>{goto_media_page(content.id,'movie')}} on:keyup={(e)=>{goto_media_page_keyHandler(content.id,'movie',e)}} class="snap-center shrink-0 w-fit text-center cursor-pointer" >
                     <img class="h-44 w-32 2xl:h-64 2xl:w-44 rounded-3xl drop-shadow-lg" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} />
                     <div class="h-12 w-32 2xl:w-36 mt-4 mb-1  bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
                             <div class="p-0 m-0 text-xs text-center">{content.title}</div>
@@ -34,7 +38,7 @@
                 </div>  
             {/if}
             {#if content.media_type == "tv" && tv_or_movie == "tv"}
-                <div role="button" tabindex="-1" on:click={()=>{goto_media_page(content.id,'tv')}} on:keyup|preventDefault={()=>{goto_media_page(content.id,'tv')}} class="cursor-pointer snap-center shrink-0 w-fit text-center" >
+                <div role="button" tabindex="-1" on:click={()=>{goto_media_page(content.id,'tv')}} on:keyup={(e)=>{goto_media_page_keyHandler(content.id,'tv',e)}} class="cursor-pointer snap-center shrink-0 w-fit text-center" >
                     <img class="h-44 w-32 lg:h-48 lg:w-36 2xl:h-64 2xl:w-44 rounded-3xl drop-shadow-lg" alt="The project logo" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} />
                     <div class="h-12 w-32 mt-4 mb-1 lg:w-36 2xl:w-44 bg-opacity-50 rounded-2xl p-1  flex items-start justify-center text-left">
                             <div class="p-0 m-0 text-xs">{content.name}</div>
