3425fb2ac4c8127d6b6cd5bf2cd19de1067f2e7f
Shomegie <shomegie@gmail.com>
2023-08-31 22:42:14 -0230

    extrapolate details from data

--- src/routes/tv/[slug]/+page.svelte
+++ src/routes/tv/[slug]/+page.svelte
@@ -9,3 +9,22 @@
     </div>
 
 </div> -->
+<div class="">
+    <div class="flex">
+        <div class="details">
+            <p class="">{data.name}</p>
+            <p class="">Rating: {parseFloat(data.vote_average).toFixed(1)}</p>
+            <p class="">Seasons: {data.seasons.length}</p>
+            <p class="">Genre:{data.genres.map((x)=> " "+x.name)}</p>
+            <p class="">Year: {data.first_air_date.substring(0,4)}</p>
+            <p class="">Language: {[...data.languages]}</p>
+            <p class="">Type: {data.type}</p>
+            <p class="">Networks:{data.networks.map((x)=>" "+x.name)}</p>
+        </div>
+        <div class="poster"></div>
+    </div>
+</div>
+
+<!-- <div>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+</div> -->
