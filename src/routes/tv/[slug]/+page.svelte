6c3e8dd7131b2ef8331073e5e40903774008d0e9
Shomegie <shomegie@gmail.com>
2023-09-01 12:31:32 -0230

    get minimum information for details on individual tv shows

--- src/routes/tv/[slug]/+page.svelte
+++ src/routes/tv/[slug]/+page.svelte
@@ -9,24 +9,27 @@
     </div>
 
 </div> -->
-<div class="">
-    <div class="flex">
+<div class="w-fit mx-auto">
+    <div class="flex space-x-10">
         <div class="details">
-            <p class="">{data.name}</p>
+            <p class="">Title: {data.name}</p>
             <p class="">Rating: {parseFloat(data.vote_average).toFixed(1)}</p>
             <p class="">Seasons: {data.seasons.length}</p>
             <p class="">Genre:{data.genres.map((x)=> " "+x.name)}</p>
             <p class="">Year: {data.first_air_date.substring(0,4)}</p>
+            <p class="">Start date: {data.first_air_date}</p>
             <p class="">Language: {[...data.languages]}</p>
             <p class="">Type: {data.type}</p>
             <p class="">Networks:{data.networks.map((x)=>" "+x.name)}</p>
+            <p class="">Status: {data.status}</p>
+            <p class="">Runtime: {data.episode_run_time[0]} minutes</p>
         </div>
         <div class="poster">
-            <img src={`https://www.themoviedb.org/t/p/original${data.poster_path}`} alt="">
+            <img class="h-44" src={`https://www.themoviedb.org/t/p/original${data.poster_path}`} alt="">
         </div>
     </div>
 </div>
 
-<div>
+<!-- <div>
     <pre>{JSON.stringify(data, null, 2)}</pre>
-</div>
+</div> -->
