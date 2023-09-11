b2fe8b80b9bcd49691404e4fb8f77d8522d98010
Shomegie <shomegie@gmail.com>
2023-09-11 08:59:25 -0230

    edit layout of indv movie page

--- src/routes/movie/[slug]/+page.svelte
+++ src/routes/movie/[slug]/+page.svelte
@@ -12,41 +12,31 @@
 
 </script>
 
-<div class="w-fit mx-auto">
-    <div class="flex space-x-10">
-        <div class="details">
-            <p class="">Title: {data.title}</p>
-            <p class="">Rating: {parseFloat(data.vote_average).toFixed(1)}</p>
+<div class="w-full mx-auto">
+    <div class="flex space-x-10 bg-red-500">
+        <div class="details w-3/5 pl-6">
+            <p class="h2 font-bold">Title: {data.title}</p>
+            <p class="h4">{data.release_date.substring(0,4)}</p>
             <p class="">Genre:{data.genres.map((x)=> " "+x.name)}</p>
-            <p class="">Year: {data.release_date.substring(0,4)}</p>
+            <p class="">Rating: {parseFloat(data.vote_average).toFixed(1)}</p>
             <p class="">Language: {data.spoken_languages.map((language)=> " " + language.english_name)}</p>
             <p class="">Production:{data.production_companies.map((production_company)=> " "+ production_company.name )}</p>
             <p class="">Status: {data.status}</p>
             <p class="">Runtime: {data.runtime} minutes</p>
-
-
-            
-
-
-
-
-
-            
-
 <!-- 
             <p class="">Type: {data.type}</p>
             <p class="">Networks:{data.networks.map((x)=>" "+x.name)}</p>
              -->
         </div>
-        <div class="poster">
+        <div class="poster bg-green-500 w-2/5">
             <img class="h-44" src={`https://www.themoviedb.org/t/p/original${data.poster_path}`} alt={data.title}>
-            {#if data.belongs_to_collection}
+            <!-- {#if data.belongs_to_collection}
                 <img class="h-44" src={`https://www.themoviedb.org/t/p/original${data.belongs_to_collection.poster_path}`} alt={data.title}>
-            {/if}
+            {/if} -->
 
         </div>
     </div>
-    <div class="similar-movies">
+    <!-- <div class="similar-movies">
         <h1 class="h3 underline">Similar movies</h1>
         <div class="flex space-x-6">
             {#each data.similar.results as similar (similar.id)}
@@ -57,7 +47,7 @@
             </div>
             {/each}
          </div>
-    </div>
+    </div> -->
 </div>
 <!-- 
 <div>
