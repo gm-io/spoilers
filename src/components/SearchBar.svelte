b0afde189baf8d8428a2173059f56a12bedac85f
Shomegie <shomegie@gmail.com>
2023-09-22 03:00:06 -0230

    create search page route

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,17 +1,12 @@
-<!-- <form>   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
-        </div>
-        <div class="bg-yellow-500 ">   
-            <input type="search" id="default-search" class="block w-10/12 p-4 py-2 pl-10 text-sm text-gray-900 rounded-2xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="search ..." required>
-            <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-        </div>
-    </div>
-</form> -->
+<script>
+    import { goto } from "$app/navigation";
+    let query = '';
+
+    let SEARCH_QUERY = () =>{
+      console.log("")
+    }
+
+</script>
 
 <div class="w-2/4 mx-auto mb-4 pt-0">
     <form action="">
