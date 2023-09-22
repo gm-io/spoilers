3e0da834201719b9c52232f852f522aa01535b6d
Gerard Magla <gmagladev@gmail.com>
2023-09-22 18:22:59 +0000

    Refactor movie page load function to fetch both movie data and credits data

--- src/routes/movie/[slug]/+page.js
+++ src/routes/movie/[slug]/+page.js
@@ -4,7 +4,7 @@ import {error} from '@sveltejs/kit';
 export async function load({fetch, params}) {
     // const movie_id = parseInt(params.slug, 10)
     const movie_id = params.slug
-    let tmdb_response;
+    let movie_data, credits_data;
 
     await fetch(`../../../api/tmdb/movie/${movie_id}`)
         .then(response => {
@@ -14,14 +14,29 @@ export async function load({fetch, params}) {
             return response.json(); // Parse response body as JSON
         })
         .then(data => {
-            tmdb_response = data;
+            movie_data = data;
         })
         .catch(error => {
             console.error('Fetch error:', error);
         });
 
-    if (tmdb_response) {
-        return tmdb_response;
+    await fetch(`../../../api/tmdb/movie/${movie_id}/credits`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Couldn\'t fetch credits');
+            }
+            return response.json(); // Parse response body as JSON
+        })
+        .then(data => {
+            credits_data = data;
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+
+
+    if (movie_data) {
+        return {...movie_data, credits: credits_data};
     }
     throw error(404, 'Not found');
 }
