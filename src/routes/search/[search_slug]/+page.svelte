cf2c3c890be6014953589b402392c2d3940c9e26
Shomegie <shomegie@gmail.com>
2023-09-23 02:13:49 -0230

    cosmetic changes to search results page

--- src/routes/search/[search_slug]/+page.svelte
+++ src/routes/search/[search_slug]/+page.svelte
@@ -3,17 +3,23 @@
     console.log(data.status)
     console.log(data)
 
-    let page_1 = data.search_data.results
+    $:page_1 = data.search_data.results
 </script>
 
 <!--separate all, tv, movies -->
-<div class="bg-yellow-600 w-9/12 mt-20 mx-auto">
-    <div class="grid grid-cols-5 gap-y-4">
-    {#each page_1 as content}
-        <div class="h-72 w-48 bg-black">
-            <div class="">{content.name}</div>
-        </div>
-    {/each}
+<div class=" w-9/12 mt-20 mx-auto">
+    <div class="grid grid-cols-5 gap-y-4 justify-items-center">
+    {#key page_1}
+        {#each page_1 as content}
+            <div class="h-fit w-48 ">
+                <!-- <img class="h-56 w-44 border-4 border-white rounde cursor-pointer" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} alt={content.title}> -->
+                <img class="h-80 border-4 border-white rounded-lg cursor-pointer" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} alt={content.title}>
+
+                <div class="">{content.name}</div>
+            </div>
+        {/each}
+    {/key}
+
     </div>
 
 </div>
