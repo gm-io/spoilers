6e5218b5de82ed08525189d96a1aecbd969d0aa2
Shomegie <shomegie@gmail.com>
2023-09-24 07:42:32 -0230

    redesign search bar

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -16,8 +16,14 @@
     <div class="absolute right-0 h-full flex items-center pr-3 pt-1">
         <LightSwitch />
     </div>
-    <div class="w-4/6 2xl:w-4/6 pl-2 2xl:pl-0 mx-auto  h-full">
-        <input bind:value={query} on:keyup={SEARCH_QUERY} type="text" id="default-search" class="block rounded-2xl w-full p-4 py-2 pl-4 md:pl-10 text-sm text-gray-900 rounded-4xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="search dis ..." required>
+    <div class="w-4/6 2xl:w-4/6 pl-2 2xl:pl-0 mx-auto relative  h-full">
+        <div class="absolute top-0 bottom-0 my-auto flex pl-3">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 m-auto text-stone-500">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>              
+        </div>
+        <input bind:value={query} on:keyup={SEARCH_QUERY} type="text" id="default-search" class="block rounded-2xl w-full p-4 py-2 pl-4 md:pl-10 text-sm text-gray-900 rounded-4xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-primary-200 dark:border-gray-600 dark:placeholder-gray-400 font-bold dark:text-black dark:placeholder-black" placeholder="" required>
+        <!-- <input bind:value={query} on:keyup={SEARCH_QUERY} type="text" id="default-search" class="block rounded-2xl w-full p-4 py-2 pl-4 md:pl-10 text-sm text-gray-900 rounded-4xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="search dis ..." required> -->
     </div>
 </div>
 <!-- <div class="h-5 w-9/12 bg-opacity-50 my-3 mt-1 mx-auto bg-black"></div> -->
