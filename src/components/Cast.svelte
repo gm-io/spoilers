0e29803f8c697b19ce5c1eaedc245f4207756bf2
Shomegie <shomegie@gmail.com>
2023-09-26 07:42:17 -0230

    1st design option for cast

--- src/components/Cast.svelte
+++ src/components/Cast.svelte
@@ -1,16 +1,40 @@
 <script>
     export let data;
     
-    const cast_10 = data.cast.splice(0,11)
+    let cast_10 = data.cast.slice(0,11)
 </script>
 
-<div class="bg-orange-500 w-full">
+<!-- <div class="border-gray-300 border-opacity-10 border-b mt-6"></div>
+<div class="cast flex flex-col">
+    <h1 class="text-gray-800 dark:text-gray-400 dark:text-opacity-70 my-2 pl-4">Cast</h1>
+    <div class="grid grid-cols-6 gap-4">
+        {#key cast_10}
+            {#each cast_10 as cast}
+                <div class="flex flex-col border-2 rounded border-black">
+                    {#if cast.profile_path}
+                        <img class="rounded-full h-10 w-10" src={`https://image.tmdb.org/t/p/original${cast.profile_path}`} alt={`${cast.name} profile`}>
+                    {/if}
+                    <div class="">{cast.name}</div>
+                    <div class="">{cast.character}</div>
+                </div>
+            {/each}
+        {/key}
+    </div>
+</div> -->
+
+
+<div class="w-full flex flex-col">
+    <div class="border-gray-300 border-opacity-10 border-b mt-6"></div>
+    <h1 class="text-gray-800 dark:text-gray-400 dark:text-opacity-70 my-2 pl-4">Cast</h1>
     <div class="grid grid-cols-5 gap-4">
         {#each cast_10 as cast}
-            <div class="flex flex-col border-2 rounded-lg border-black">
-                <img src={`https://image.tmdb.org/t/p/original${cast.profile_path}`} alt={`${cast.name} profile`}>
-                <div class="">{cast.name}</div>
-                <div class="">{cast.character}</div>
+            <div class="flex bg-white bg-opacity-80 text-black">
+                <img class="h-32 rounded border object-cover border-white border-opacity-10" src={`https://image.tmdb.org/t/p/original${cast.profile_path}`} alt={`${cast.name} profile`}>
+                <div class="flex flex-col">
+                    <div class="">{cast.name}</div>
+                    <div class="">{cast.character}</div>
+                </div>
+
             </div>
             
         {/each}
@@ -19,6 +43,6 @@
 
 
 
-<div>
+<!-- <div>
     <pre>{JSON.stringify(data.cast, null, 2)}</pre>
-</div>
+</div> -->
