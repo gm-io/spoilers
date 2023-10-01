10f08995515cc5a309e9fe2874d31853f6b5f419
Shomegie <shomegie@gmail.com>
2023-10-01 10:42:34 -0230

    make responsive: search page

--- src/routes/search/[search_slug]/+page.svelte
+++ src/routes/search/[search_slug]/+page.svelte
@@ -13,19 +13,19 @@
 </script>
 
 <!--separate all, tv, movies -->
-<div class=" w-full md:w-9/12 mt-20 mx-auto">
-    <div class="pl-4">
+<div class=" w-full lg:w-9/12 mt-20 mx-auto">
+    <div class="pl-2">
         Search for: {data.query}
     </div>
     <div class="border-gray-300 w-11/12 mx-auto border-opacity-5 border-b my-6"></div>
-    <div class="grid grid-cols-2 md:grid-cols-5 gap-y-8 justify-items-center bg-green-500 max-w-full">
+    <div class="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-y-8 justify-items-center max-w-full">
     {#key page_1}
         {#each page_1 as content}
-            <div on:click={()=>{goto_media_page(content.id,content.media_type)}} class="h-fit w-48 ">
-                <!-- <img class="h-56 w-44 border-4 border-white rounde cursor-pointer" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} alt={content.title}> -->
-                <img class="h-64 w-44 border-4 border-primary-300 dark:border-opacity-20 border-opacity-40 rounded cursor-pointer" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} alt={content.title}>
-
-                <div class="w-44 text-center pt-2 text-sm px-2 font-bold">{content.media_type=="tv" ? content.name : content.title}</div>
+            <div on:click={()=>{goto_media_page(content.id,content.media_type)}} class="h-fit w-fit bg-orange  flex justify-center ">
+                <div class="">
+                    <img class="h-52 w-36 xl:h-64 xl:w-48 border-4 border-white dark:border-opacity-70 border-opacity-40 rounded cursor-pointer" src={`https://www.themoviedb.org/t/p/original${content.poster_path}`} alt={content.title}>
+                    <div class="w-36 xl:w-48 text-center pt-2 text-sm px-2 font-bold">{content.media_type=="tv" ? content.name : content.title}</div>
+                </div>
             </div>
         {/each}
     {/key}
