472b74add9a1c3f26bef6775a2c1f5017a07e83a
Gerard Magla <gmagladev@gmail.com>
2023-10-14 20:52:41 +0000

    Add "appears_in to person endpoint

--- src/routes/api/tmdb/person/[slug]/+server.js
+++ src/routes/api/tmdb/person/[slug]/+server.js
@@ -1,22 +1,21 @@
-import {json, error} from "@sveltejs/kit";
-import { env } from "$env/dynamic/private"
+import {json} from "@sveltejs/kit";
+import {env} from "$env/dynamic/private"
 
 //relevant tmdb docs: https://developer.themoviedb.org/reference/tv-series-details
 
 const tmdb_access_token = env.TMDB_ACCESS_TOKEN;
-export async function GET({params}){
+
+export async function GET({params}) {
 
     const person_id = params.slug;
 
     const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: `Bearer ${tmdb_access_token}`
+        method: 'GET', headers: {
+            accept: 'application/json', Authorization: `Bearer ${tmdb_access_token}`
         }
     };
 
-    const data = await fetch(`https://api.themoviedb.org/3/person/${person_id}`, options)
+    const main_data = await fetch(`https://api.themoviedb.org/3/person/${person_id}`, options)
         .then(res => res.json())
         .then(data => {
             return data;
@@ -24,5 +23,10 @@ export async function GET({params}){
         .catch(err => {
             return err;
         })
-    return json(data);
-    }
+    const credits_data = await fetch(`https://api.themoviedb.org/3/person/${person_id}/combined_credits`, options)
+        .then(res => res.json())
+        .then(data => {
+            return data;
+        })
+    return json({...main_data, appears_in: credits_data});
+}
