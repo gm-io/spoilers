ece4fe8296a49e2ed729d4bcb8adc6db70f96f75
Shomegie <shomegie@gmail.com>
2023-10-23 08:57:36 -0230

    list other media that person appears in

--- src/routes/person/[slug]/+page.svelte
+++ src/routes/person/[slug]/+page.svelte
@@ -14,6 +14,18 @@ this is a person page
 <div class="">Department: {person.known_for_department}</div>
 
 <div class="border-b border-4 border-black w-full my-2"></div>
+<div class=""></div>
+<div class="grid grid-cols-3 gap-2">
+    {#each person.appears_in.cast as role }
+        <div class="flex flex-col bg-black bg-opacity-20">
+            <div class="font-bold">{role.media_type=="movie"?role.title:role.name}</div>
+
+            <div class="text-xs">As {role.character}</div>
+        </div>
+    {/each}
+</div>
+<div class="border-b border-4 border-black w-full my-2"></div>
+
 
 <div>
     <pre>{JSON.stringify(data, null, 2)}</pre>
