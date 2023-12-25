a111cc3d9b6e9d81288c360a27f8a70e46355deb
Shomegie <shomegie@gmail.com>
2023-12-25 07:33:35 -0330

    skeleton layout for person page design

--- src/routes/person/[slug]/+page.svelte
+++ src/routes/person/[slug]/+page.svelte
@@ -6,10 +6,14 @@
 </script>
 
 
+<div class="w-full px-0 md:px-4 lg:px-0 lg:w-9/12 2xl:w-9/12 mx-auto mt-6 md:mt-10 2xl:mt-12 ">
+    <div class="bg-black w-full h-[400px]">
+        <div class="pl-10 h-full w-fit flex items-center">
+            <img class=" h-72 rounded  border-white" src={`https://www.themoviedb.org/t/p/original${person.profile_path}`} alt={person.name}>
+        </div>
+    </div>
+</div>
 
-
-
-<!-- <div class="overflow-x-hidden">
-    <div class="h-60 w-full bg-black"></div>
+<div class="overflow-x-hidden">
     <pre>{JSON.stringify(data, null, 2)}</pre>
-</div> -->
+</div>
